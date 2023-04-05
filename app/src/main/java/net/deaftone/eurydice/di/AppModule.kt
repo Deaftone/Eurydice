b0d837738161b7e535ae4bfcb48ff10b59c67427
Rylee George <ryleegeorge1@gmail.com>
2023-04-06 09:26:59 +1000

    Add AppModule

---
+++ app/src/main/java/net/deaftone/eurydice/di/AppModule.kt
@@ -0,0 +1,10 @@
+package net.deaftone.eurydice.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+}
