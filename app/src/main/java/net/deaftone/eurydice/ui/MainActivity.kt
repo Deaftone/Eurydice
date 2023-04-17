981a98072c2b0c39c137b0fce5cd76aacf90e147
Rylee George <ryleegeorge1@gmail.com>
2023-04-17 11:49:57 +1000

    ktlint file

--- app/src/main/java/net/deaftone/eurydice/ui/MainActivity.kt
+++ app/src/main/java/net/deaftone/eurydice/ui/MainActivity.kt
@@ -8,11 +8,12 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -21,15 +22,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import net.deaftone.eurydice.R
-
 import net.deaftone.eurydice.ui.navigation.BottomBar
-import net.deaftone.eurydice.ui.navigation.BottomBarScreen
 import net.deaftone.eurydice.ui.navigation.MainScreenNavGraph
 import net.deaftone.eurydice.ui.navigation.MainScreenRoutes
-import net.deaftone.eurydice.ui.widget.TopAppBar
 import net.deaftone.eurydice.ui.theme.EurydiceTheme
+import net.deaftone.eurydice.ui.widget.TopAppBar
+
+
 @OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -78,7 +80,7 @@ fun NavScreen(navController: NavHostController = rememberAnimatedNavController()
             visible = topBarDestination,
             enter = slideInVertically(animationSpec = spring(visibilityThreshold = IntOffset.Zero)),
             exit = slideOutVertically(animationSpec = spring(visibilityThreshold = IntOffset.Zero))
-        ){
+        ) {
             TopAppBar(stringResource(id = R.string.app_name))
         }
     }, bottomBar = { BottomBar(navController = navController) }) { padding ->
